ac691edec1aba09e4769cf80d563a81f08e52999
Aaron Kutch <aaronkutch@att.net>
2018-06-21 13:33:09 -0500

    added more to the lib.rs docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! These division functions use a different type of long division than the binary long division
+//! typically used by software to divide integers larger than the size of the cpus native hardware
+//! division.
+
 mod all_all;
 
 pub use all_all::*;
